374b9124bda62673fbe7edcc035d1c9271d7959e
Kara Temple <kara.temple@bryanuniversity.edu>
2020-10-23 16:44:02 -0400

    checking

---
+++ week1/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox"/>
+            <p>Change Oil</p>
+
+            <input type="checkbox"/>
+            <p>Change Oil Filters</p>
+
+            <input type="checkbox"/>
+            <p>Clean Fuel Strainer</p>
+
+            <input type="checkbox"/>
+            <p>Replace Rudder Bearing</p>
+
+        </div>
+
+    )
+}
+
+export default App 
