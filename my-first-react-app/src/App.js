4c94ddac2d75ffcd1759cf0ec5cb6c5004ade61c
Kara Temple <kara.temple@bryanuniversity.edu>
2020-10-24 18:37:00 -0400

    assignment1

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -3,16 +3,16 @@ import React from "react"
 function App() {
     return (
         <div>
-            <input type="checkbox"/>
+            <input type="checkbox" />
             <p>Change Oil</p>
 
-            <input type="checkbox"/>
+            <input type="checkbox" />
             <p>Change Oil Filters</p>
 
-            <input type="checkbox"/>
+            <input type="checkbox" />
             <p>Clean Fuel Strainer</p>
 
-            <input type="checkbox"/>
+            <input type="checkbox" />
             <p>Replace Rudder Bearing</p>
 
         </div>
