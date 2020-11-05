f0eeafb59fd35f290f8c96ae4afe2d00118d3950
Kara Temple <kara.temple@bryanuniversity.edu>
2020-11-05 13:31:02 -0500

    update

--- week2/my-app/src/Spot.js
+++ week2/my-app/src/Spot.js
@@ -3,10 +3,10 @@ import React from "react"
 function Spot(props) {
     return (
        <div className="places">
+           
            <h1>Place: {props.place}</h1>
            <h2>Price in USD: {props.price}</h2>
            <h2>When to Visit: {props.timeToGo}</h2>
-           <h2>Affordability: </h2>
         
        </div>
     )
